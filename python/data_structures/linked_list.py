30a81794d5b46069ed6e020c390f5d7860def42e
Jason Christopher <jchristopher2448@gmail.com>
2022-12-27 22:11:05 -0600

    CC6 tests passed

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value, next=None):
         self.value = value
-        self.next = None
+        self.next = next
 
 
 class LinkedList:
@@ -9,16 +9,14 @@ class LinkedList:
         self.head = None
 
     def insert(self, value):
-        self.val = Node(value)
-        self.val.next = self.head
-        self.head = self.val
+        self.head = Node(value, self.head)
 
     def includes(self, value):
         current = self.head
         while current is not None:
             if current.value == value:
                 return True
-        current = current.next
+            current = current.next
         return False
 
     def __str__(self):
@@ -29,6 +27,37 @@ class LinkedList:
             current = current.next
         return string + "NULL"
 
+    def append(self, value):
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = Node(value)
 
-class TargetError:
+    def insert_before(self, before, value):
+        current = self.head
+        previous = None
+        try:
+            while current.value is not before:
+                previous = current
+                current = current.next
+            if previous is not None:
+                previous.next = Node(value, current)
+            if previous is None:
+                self.head = Node(value, current)
+        except Exception as e:
+            raise TargetError(e)
+
+    def insert_after(self, after, value):
+        current = self.head
+        try:
+            while current.value is not after:
+                current = current.next
+            new_node = Node(value)
+            new_node.next = current.next
+            current.next = new_node
+        except Exception as e:
+            raise TargetError(e)
+
+
+class TargetError(Exception):
     pass
