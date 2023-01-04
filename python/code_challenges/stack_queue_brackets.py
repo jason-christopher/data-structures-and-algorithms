707680bb37c4490cbf3a28ff72ee307c0ce8b2e5
Jason Christopher <jchristopher2448@gmail.com>
2023-01-04 16:35:29 -0600

    simplified CC13

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -5,11 +5,8 @@ def multi_bracket_validation(string):
     stack, opened, closed = Stack(), ["{", "[", "("], ["}", "]", ")"]
     for x in string:
         stack.push(x) if x in opened else None
-        if x in closed:
-            if stack.is_empty():
-                return False
-            top = stack.pop()
-            if opened.index(top) != closed.index(x):
+        if x in closed and not stack.is_empty():
+            if opened.index(stack.pop()) != closed.index(x):
                 return False
     if stack.is_empty():
         return True
