38360eedb9070741833253196eb7e5687d155de4
Jason Christopher <jchristopher2448@gmail.com>
2023-01-06 17:22:53 -0600

    passed binary trees tests

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,34 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
-    """
-    Put docstring here
-    """
+    def add(self, value):
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+            return
+        node = self.root
+        while True:
+            if value < node.value:
+                if not node.left:
+                    node.left = new_node
+                    break
+                node = node.left
+            elif value > node.value:
+                if not node.right:
+                    node.right = new_node
+                    break
+                node = node.right
+            else:
+                break
 
-    def __init__(self):
-        # initialization here
-        pass
-
-    def some_method(self):
-        # method body here
-        pass
+    def contains(self, value):
+        node = self.root
+        while node:
+            if value < node.value:
+                node = node.left
+            elif value > node.value:
+                node = node.right
+            else:
+                return True
+            return False
--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,16 +1,49 @@
-class BinaryTree:
-    """
-    Put docstring here
-    """
-
-    def __init__(self):
-        # initialization here
-        pass
-
-    def some_method(self):
-        # method body here
-        pass
-
-
 class Node:
-    pass
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class BinaryTree:
+    def __init__(self, root=None):
+        self.root = root
+
+    def pre_order(self, root=None, nodes=None):
+        if root is None:
+            root = self.root
+        if nodes is None:
+            nodes = []
+        nodes.append(root.value)
+        if root.left:
+            self.pre_order(root.left, nodes)
+        if root.right:
+            self.pre_order(root.right, nodes)
+        return nodes
+
+    def in_order(self, root=None, nodes=None):
+        # used in binary search trees
+        if root is None:
+            root = self.root
+        if nodes is None:
+            nodes = []
+        if root.left:
+            self.in_order(root.left, nodes)
+        nodes.append(root.value)
+        if root.right:
+            self.in_order(root.right, nodes)
+        return nodes
+
+    def post_order(self, root=None, nodes=None):
+        # used for deleting leaves/nodes from the bottom up
+        if root is None:
+            root = self.root
+        if nodes is None:
+            nodes = []
+        if root.left:
+            self.post_order(root.left, nodes)
+        if root.right:
+            self.post_order(root.right, nodes)
+        nodes.append(root.value)
+        return nodes
+
