e41d57352d561eec77cdab0706ecae44497bd69f
Jason Christopher <jchristopher2448@gmail.com>
2023-01-10 10:51:50 -0600

    need to copy original tree

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,33 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree, Node
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(tree):
+    try:
+        # new_tree = tree
+        new_node = Node(tree.root.value)
+        new_node.children = tree.root.children.copy()
+        new_tree = KaryTree(new_node)
+
+        def fizz_buzz(root):
+            if not root:
+                return None
+            if root.value % 3 == 0 and root.value % 5 == 0:
+                root.value = "FizzBuzz"
+            elif root.value % 3 == 0:
+                root.value = "Fizz"
+            elif root.value % 5 == 0:
+                root.value = "Buzz"
+            else:
+                root.value = str(root.value)
+            for child in root.children:
+                fizz_buzz(child)
+            return root
+
+        new_tree.root = fizz_buzz(new_tree.root)
+
+        return new_tree
+    except:
+        return KaryTree()
+
+
--- python/data_structures/kary_tree.py
+++ python/data_structures/kary_tree.py
@@ -6,19 +6,30 @@ class KaryTree:
         self.root = root
 
     def breadth_first(self):
-        queue = Queue()
+        # queue = Queue()
+        #
+        # collection = []
+        #
+        # queue.enqueue(self.root)
+        #
+        # while not queue.is_empty():
+        #     node = queue.dequeue()
+        #     collection.append(node.value)
+        #     for child in node.children:
+        #         queue.enqueue(child)
+        #
+        # return collection
 
-        collection = []
-
-        queue.enqueue(self.root)
-
-        while not queue.is_empty():
-            node = queue.dequeue()
-            collection.append(node.value)
-            for child in node.children:
-                queue.enqueue(child)
-
-        return collection
+        try:
+            result, queue = [], [self.root]
+            while queue:
+                front = queue.pop(0)
+                result.append(front.value)
+                for child in front.children:
+                    queue.append(child)
+            return result
+        except:
+            return []
 
 
 class Node:
