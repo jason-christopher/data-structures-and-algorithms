8f398b6fb2b71bd180ac18d390a0af5e4567cca7
Jason Christopher <jchristopher2448@gmail.com>
2023-01-31 12:12:26 -0600

    CC33 code complete, need more tests and README

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,8 @@
-from data_structures.hashtable import Hashtable
-
-
-def left_join():
-    pass
+def left_join(dictionary1, dictionary2):
+    result = []
+    for key, value in dictionary1.items():
+        if key in dictionary2:
+            result.append([key, value, dictionary2.pop(key)])
+        else:
+            result.append([key, value, "NONE"])
+    return result
