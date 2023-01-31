4af33cc1277dad650dea0c0a730a1f88db29d9b1
Jason Christopher <jchristopher2448@gmail.com>
2023-01-31 12:53:58 -0600

    CC32 complete, changed to dictionary, passing new tests

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,20 +1,15 @@
-from data_structures.binary_tree import BinaryTree
-from data_structures.hashtable import Hashtable
-
-
 def tree_intersection(tree1, tree2):
+    result, dict = [], {}
     def walk(root, is_first_tree):
-        if is_first_tree:
-            diction.set(root.value, 1)
-        if diction.has(root.value) and not is_first_tree:
+        if is_first_tree and root.value not in dict:
+            dict[root.value] = 1
+        if root.value in dict and not is_first_tree:
             result.append(root.value)
         if root.left:
             walk(root.left, is_first_tree)
         if root.right:
             walk(root.right, is_first_tree)
-    diction, result = Hashtable(), []
-    if tree1.root:
+    if tree1.root and tree2.root:
         walk(tree1.root, True)
-    if tree2.root:
         walk(tree2.root, False)
     return set(result)
