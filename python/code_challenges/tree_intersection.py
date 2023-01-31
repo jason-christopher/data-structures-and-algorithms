3b3446e48aeb6ee04c98bb73aa4c160f77af96d8
Jason Christopher <jchristopher2448@gmail.com>
2023-01-30 20:35:40 -0600

    CC32 complete, whiteboard and tests added

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,20 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree1, tree2):
+    def walk(root, is_first_tree):
+        if is_first_tree:
+            diction.set(root.value, 1)
+        if diction.has(root.value) and not is_first_tree:
+            result.append(root.value)
+        if root.left:
+            walk(root.left, is_first_tree)
+        if root.right:
+            walk(root.right, is_first_tree)
+    diction, result = Hashtable(), []
+    if tree1.root:
+        walk(tree1.root, True)
+    if tree2.root:
+        walk(tree2.root, False)
+    return set(result)
